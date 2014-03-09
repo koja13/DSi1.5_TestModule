a63c9885c67d07fd467c246c5d46cb5c8380aee2
Dejan Todosijević <dejantodosijevic@outlook.com>
2014-03-09 01:25:31 +0100

    Komitovanje 09.03.2014

--- assets/js/fbLoginScript.js
+++ assets/js/fbLoginScript.js
@@ -1,7 +1,7 @@
   window.fbAsyncInit = function() {
   FB.init({
     appId      : '128303734043111', // App ID
-    channelUrl : 'http://www.martin.rs/DSi2.0_TestModule/', // Channel File
+    channelUrl : 'http://www.martin.rs/DSi1.5_TestModule/', // Channel File
     status     : true, // check login status
     cookie     : true, // enable cookies to allow the server to access the session
     xfbml      : true  // parse XFBML
